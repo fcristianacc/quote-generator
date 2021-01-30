4f683604ce95bff4f0eef3fc3fdf45b8a8f0c846
fcristianacc <fcristianacc@gmail.com>
2021-01-30 12:30:11 +0000

    Created a button to get new quotes from an API

---
+++ script.js
@@ -0,0 +1,43 @@
+const quoteContainer = document.getElementById("quote-container");
+const quoteText = document.getElementById("quote");
+const quoteAuthor = document.getElementById("author");
+
+let apiQuotes = [];
+
+// Show New Quote
+function newQuote() {
+  // Pick a Random Quote from the apiQuotes Array
+  const quote = apiQuotes[Math.floor(Math.random() * apiQuotes.length)];
+  console.log(quote);
+
+  if (!quote.author) {
+    quoteAuthor.textContent = "Unknown";
+  } else {
+    quoteAuthor.textContent = quote.author;
+  }
+
+  //Check Quote Length to determine the styling
+  if (quote.text.length > 240) {
+    quoteText.classList.add("long-quote");
+  } else {
+    quoteText.classList.remove("long-quote");
+  }
+  quoteText.textContent = quote.text;
+}
+
+// Get Quotes From API
+async function getQuote() {
+  const apiUrl = "https://type.fit/api/quotes";
+
+  try {
+    const response = await fetch(apiUrl);
+    apiQuotes = await response.json();
+    newQuote();
+  } catch (error) {
+    // Catch Error Here
+    console.log("Whoops, no quote!", error);
+  }
+}
+
+// On Load
+getQuote();
